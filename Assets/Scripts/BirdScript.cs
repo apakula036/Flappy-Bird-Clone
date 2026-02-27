994e25abd5f2e8e6926d57257df6379b0efa9899
Andrew <70990567+apakula036@users.noreply.github.com>
2026-02-26 21:04:35 -0800

    massive update

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 
 public class BirdScript : MonoBehaviour
 {
+    public GameObject Death_Explosion; 
     public Rigidbody2D myRigidbody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -23,25 +26,25 @@ public class BirdScript : MonoBehaviour
         if (Keyboard.current.leftArrowKey.isPressed)
         {
             //horizontal = -1.0f;
-            myRigidbody.linearVelocity = Vector2.left * 10;
+            myRigidbody.linearVelocity = Vector2.left * 5;
         }
         else if (Keyboard.current.rightArrowKey.isPressed)
         {
-            myRigidbody.linearVelocity = Vector2.right * 10;
+            myRigidbody.linearVelocity = Vector2.right * 5;
         }
 
 
         //float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
+        else if (Keyboard.current.upArrowKey.isPressed)
         {
             //vertical = 1.0f;
-            myRigidbody.linearVelocity = Vector2.up * 10;
+            myRigidbody.linearVelocity = Vector2.up * 7;
         }
-        else if (Keyboard.current.downArrowKey.isPressed)
+        /* else if (Keyboard.current.downArrowKey.isPressed)
         {
             //vertical = -1.0f;
             myRigidbody.linearVelocity = Vector2.down * 10;
-        }
+        } */ 
 
 
         //Vector2 position = transform.position;
@@ -49,4 +52,10 @@ public class BirdScript : MonoBehaviour
         //position.y = position.y + 0.1f * vertical;
         //transform.position = position;
     }
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        Destroy(gameObject);
+        Instantiate(Death_Explosion, transform.position, transform.rotation);
+    }
 }
+
