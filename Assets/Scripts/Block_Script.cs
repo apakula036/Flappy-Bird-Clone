6c03b9a5801bb8af0c4fc35e00175e80b9632d30
Andrew <70990567+apakula036@users.noreply.github.com>
2026-03-02 20:47:45 -0800

    Fixed block logic, added score in UI, added restart button,in UI added moving scenery

--- Assets/Scripts/Block_Script.cs
+++ Assets/Scripts/Block_Script.cs
@@ -9,51 +9,118 @@ public class Block_Script : MonoBehaviour
 {
     public GameObject Death_Explosion; 
     public Rigidbody2D myRigidbody;
+    public Rigidbody2D MovingBlockOne;
+    public Rigidbody2D MovingBlockTwo;
+    public Rigidbody2D MovingBlockThree;
+    public Rigidbody2D MovingBlockFour;
     public Vector3 localScale;
+    public float xScaleValue = 1.475f;
+    public float minValue = 7.9f;
+    public float maxValue = 0f;
+    public float medianValue = 10f; 
+    public float rightSpawnPosition = 30f;
+    public float bottomSpawnYValue = -7f;
+    public float topSpawnYValue = 7.71f;
+    public float pointToRespawnBlocks = -21f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.name = "block";
-        //moveBlocks();
+        //MovingBlockOne.name = "blockOne";
+        MoveBlocks();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnBlocks();
+        CheckForBlockOne();
     }
 
-    void spawnBlocks(){
+    void CheckForBlockOne(){
         Vector2 position = transform.position;
-        if (position.x < -25){
-        int sizeRangeForBlock = Random.Range(1,15);
-        position.x = 21;
-        transform.localScale = new Vector3(sizeRangeForBlock, sizeRangeForBlock, sizeRangeForBlock);
-        transform.position = position;
-        }
+        float sizeYRangeForBlock = Random.Range(8.3f,13.5f);
+        float sizeXRangeForBlock = Random.Range(1.1f,3f);
+        if (position.x < pointToRespawnBlocks)
+        {
+            position.x = rightSpawnPosition;
+            transform.localScale = new Vector3(sizeXRangeForBlock, sizeYRangeForBlock, 1);
+            transform.position = position;
+            MoveBlocks();
+        } 
+    }
+    void MoveBlocks(){
+        //move the blocks to the left
         myRigidbody.linearVelocity = Vector2.left * 9;
-
-        
     }
-
-    void moveBlocks(){
-        myRigidbody.linearVelocity = Vector2.left * 10;
-    }
-
     void OnCollisionEnter2D(Collision2D collision) 
+    {
+        //when it crashes into the backwall it explodes 
+        Destroy(gameObject);
+        Instantiate(Death_Explosion, transform.position, transform.rotation);
+    }
+
+    
+}
+/** -----------NOTES SECTION -------------------------------
+
+void OnCollisionEnter2D(Collision2D collision) 
     {
         Destroy(gameObject);
         Instantiate(Death_Explosion, transform.position, transform.rotation);
     }
-}
-/** -----------NOTES SECTION -------------------------------
 
-public void spawnBlocks(){
-    spawn block 
+void randomWhatSideBlockSpawnsOn(){
+    random number 1 or 2 to pick top or bottom 
+
 }
-public void moveblocks(){
-    move
-}
+
+    // Wait for 3 seconds (scaled time)
+        //new WaitForSeconds(3);
+
+
+if (position.x < -25f && position.y > 0){
+            //check if theyre too short 
+            if (sizeYRangeForBlock >= minValue && sizeYRangeForBlock <= medianValue) { 
+                position.y = -10f;
+                position.x = rightSpawnPosition;
+                transform.localScale = new Vector3(xScaleValue, sizeYRangeForBlock, 1);
+                Debug.Log("Y > 0: " + sizeYRangeForBlock);
+                transform.position = position;
+            }
+            else if (sizeYRangeForBlock >= minValue && sizeYRangeForBlock >= medianValue) { 
+                //position.y = bottomSpawnYValue;
+                position.x = rightSpawnPosition;
+                transform.localScale = new Vector3(xScaleValue, sizeYRangeForBlock, 1);
+                Debug.Log("Y > 0 bottom logic " + sizeYRangeForBlock);
+                transform.position = position;
+            }
+        }
+
+
+        //top side blocks spawn in and touch the top of the screen 
+        else if (position.x < -25f && position.y < 0){
+            //check if theyre too short 
+            if (sizeYRangeForBlock >= minValue && sizeYRangeForBlock <= medianValue) { 
+                position.y = topSpawnYValue;
+                position.x = rightSpawnPosition;
+                transform.localScale = new Vector3(xScaleValue, sizeYRangeForBlock, 1);
+                Debug.Log("Y < 0: " + sizeYRangeForBlock);
+                transform.position = position;
+            }
+            
+            else if (sizeYRangeForBlock >= minValue && sizeYRangeForBlock >= medianValue) { 
+                //position.y = topSpawnYValue;
+                position.x = rightSpawnPosition;
+                transform.localScale = new Vector3(xScaleValue, sizeYRangeForBlock, 1);
+                Debug.Log("Y < 0 bottom logic " + sizeYRangeForBlock);
+                transform.position = position;
+            }
+        }
+
+
+
+
 
 
 
