939fa508b2de3acd200af8632e0e645f36ecf586
Andrew <70990567+apakula036@users.noreply.github.com>
2026-03-08 19:49:35 -0700

    Added main menu

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -65,12 +65,6 @@ public class BirdScript : MonoBehaviour
         score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
         scoreText.text = "Score: " + score;
     }
-    void UpdateScore()
-    {
-        elapsedTime += Time.deltaTime;
-        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
-        scoreText.text = "Score: " + score;
-    }
     void OnCollisionEnter2D(Collision2D collision) 
     {
         //check if the collision is a wall 
@@ -98,8 +92,12 @@ public class BirdScript : MonoBehaviour
 
 
 
-
-
 }
 
+/*
 
+Powerup to unlock the ability to fly left and right 
+
+
+
+*/
