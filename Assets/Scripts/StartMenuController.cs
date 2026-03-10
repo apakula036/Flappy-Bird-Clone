9364830bd356d1a8edfe44d2b5139fefc3ceb228
Andrew <70990567+apakula036@users.noreply.github.com>
2026-03-09 19:23:32 -0700

    Added background to main menu  (#6)
    * Added main menu
    
    * in progress, adding sound pieces
    
    * adding sounds
    
    * Added trees to main menu for visaul response and a better look

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -3,6 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
+    public Rigidbody2D myRigidbodyTreeMainMenu;
+    public Vector3 localScale;
+    public float rightSpawnPosition = 33f;
+    public float pointToRespawnTrees = -29f;
+    public float speedToMoveTrees = 3; 
+    void Start()
+    {
+        MoveTrees();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForTrees();
+    }
+
+    void CheckForTrees(){
+        Vector2 position = transform.position;
+        float sizeYRangeForBlock = Random.Range(.9f,1.3f);
+        float sizeXRangeForBlock = Random.Range(.9f,1.3f);
+        if (position.x < pointToRespawnTrees)
+        {
+            position.x = rightSpawnPosition;
+            transform.localScale = new Vector3(sizeXRangeForBlock, sizeYRangeForBlock, 1);
+            transform.position = position;
+            MoveTrees();
+        } 
+    }
+    void MoveTrees(){
+        //move the blocks to the left
+        myRigidbodyTreeMainMenu.linearVelocity = Vector2.left * speedToMoveTrees;
+    }
     public void OnStartClick()
     {
         SceneManager.LoadScene("SampleScene");
